682fd80786fb3f069377489864780b068c27385a
tonisco <tonyumeozulu@gmail.com>
2023-07-24 13:50:37 +0100

    sidebar

--- resources/views/components/vendor/layout/main.blade.php
+++ resources/views/components/vendor/layout/main.blade.php
@@ -9,20 +9,20 @@
 @endphp
 
 @section('content')
-    <div class="flex w-full">
-        <div class="hidden lg:flex">
+    <div class="flex flex-1 w-full h-full">
+        <div class="relative hidden lg:flex">
             <x-vendor.layout.sidebar />
         </div>
         <div class="flex flex-col flex-1">
-            <header class="flex justify-between flex-1 items-center px-4 py-2 text-white bg-red-500 dark:bg-red-700">
+            <header class="flex items-center justify-between w-full px-4 py-2 text-white bg-red-500 dark:bg-red-700">
                 <div>
-                    <x-ri-menu-fill class="h-5 sm:h-6 w-5 sm:w-6 cursor-pointer block lg:hidden" />
+                    <x-ri-menu-fill class="block w-5 h-5 cursor-pointer sm:h-6 sm:w-6 lg:hidden" />
                 </div>
                 <div class="hidden sm:block">
                     <x-general.layout.logo />
                 </div>
                 <div class="flex items-center gap-2">
-                    <img src="{{ asset($vendor->image) }}" class="object-cover w-6 sm:w-8 h-6 sm:h-8 rounded-full" />
+                    <img src="{{ asset($vendor->image) }}" class="object-cover w-6 h-6 rounded-full sm:w-8 sm:h-8" />
                     <p class="max-w-[110px] sm:text-base text-sm truncate">{{ $vendor->name }}</p>
                     <x-general.layout.theme />
                 </div>
--- resources/views/components/vendor/layout/sidebar.blade.php
+++ resources/views/components/vendor/layout/sidebar.blade.php
@@ -1,1 +1,40 @@
-sidebar
+<nav class="h-full bg-red-500 dark:bg-red-700 pt-16 flex flex-col text-white">
+    <a href="{{ route('vendor.dashboard') }}"
+        class="flex items-center gap-2 border-b border-b-gray-400/70 px-4 py-4 cursor-pointer hover:bg-red-700 dark:hover:bg-red-900 transition-all">
+        <x-ri-dashboard-line class="h-5 w-5" />
+        <p> Dashboard </p>
+    </a>
+    <a
+        class="flex items-center gap-2 border-b border-b-gray-400/70 px-4 py-4 cursor-pointer hover:bg-red-700 dark:hover:bg-red-900 transition-all">
+        <x-ri-clipboard-line class="h-5 w-5" />
+        <p> Orders </p>
+    </a>
+    <a
+        class="flex items-center gap-2 border-b border-b-gray-400/70 px-4 py-4 cursor-pointer hover:bg-red-700 dark:hover:bg-red-900 transition-all">
+        <x-ri-shopping-cart-line class="h-5 w-5" />
+        <p> Products </p>
+    </a>
+    <a
+        class="flex items-center gap-2 border-b border-b-gray-400/70 px-4 py-4 cursor-pointer hover:bg-red-700 dark:hover:bg-red-900 transition-all">
+        <x-ri-store-3-line class="h-5 w-5" />
+        <p> Shop Profile </p>
+    </a>
+    <a href="{{ route('dashboard') }}"
+        class="flex items-center gap-2 border-b border-b-gray-400/70 px-4 py-4 cursor-pointer hover:bg-red-700 dark:hover:bg-red-900 transition-all">
+        <x-ri-user-line class="h-5 w-5" />
+        <p> User Dashboard </p>
+    </a>
+    <a href="{{ route('home') }}"
+        class="flex items-center gap-2 border-b border-b-gray-400/70 px-4 py-4 cursor-pointer hover:bg-red-700 dark:hover:bg-red-900 transition-all">
+        <x-ri-home-5-line class="h-5 w-5" />
+        <p> Home Page </p>
+    </a>
+    <form method="POST" action="{{ route('logout') }}">
+        @csrf
+        <a href="{{ route('logout') }}" onclick="event.preventDefault();this.closest('form').submit();"
+            class="flex items-center gap-2 border-b border-b-gray-400/70 px-4 py-4 cursor-pointer hover:bg-red-700 dark:hover:bg-red-900 transition-all">
+            <x-ri-logout-box-line class="h-5 w-5" />
+            <p> Log Out </p>
+        </a>
+    </form>
+</nav>
