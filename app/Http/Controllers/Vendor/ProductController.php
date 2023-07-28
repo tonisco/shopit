4911eadc97ddd2f4bf4ff6d73930f664e38e9b35
tonisco <tonyumeozulu@gmail.com>
2023-07-28 14:14:08 +0100

    table created

--- app/Http/Controllers/Vendor/ProductController.php
+++ app/Http/Controllers/Vendor/ProductController.php
@@ -3,15 +3,50 @@
 namespace App\Http\Controllers\Vendor;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Yajra\DataTables\Facades\DataTables;
 
 class ProductController extends Controller
 {
 	/**
 	 * Display a listing of the resource.
 	 */
-	public function index()
+	public function index(Request $request)
 	{
+		if ($request->ajax()) {
+			$data = Product::where('vendor_id', Auth::user()->vendor->id)->get();
+			return DataTables::of($data)
+				->addIndexColumn()
+				->addColumn('action', function ($query) {
+					$edit_button = "<a href='" . route('vendor.product.edit', $query->id) . "' class='p-2 bg-blue-500 rounded cursor-pointer dark:bg-blue-700'><i class='w-4 h-4 text-white bi bi-pencil-square'></i></a>";
+
+					$delete_button = '<div>
+										<a class="p-2 bg-red-500 rounded cursor-pointer dark:bg-red-700 deleteButton"
+										data-id="' . $query->id . '" data-name="' . $query->name . '" data-route="' . route('vendor.product.destroy', $query->id) . '">
+											<i class="w-4 h-4 text-white bi bi-trash"></i>
+										</a>
+									</div>';
+
+					$options_button = '<div x-data="toggler" class="relative cursor-pointer">
+										<a @click="toggle" class="p-2 rounded bg-zinc-700">
+											<i class="w-4 h-4 text-white bi bi-three-dots-vertical"></i>
+										</a>
+										<div @click.outside="toggle" x-show="open" class="product-options">
+											<a class="product-options-item">Image Gallery</a>
+											<a class="product-options-item">Variants</a>
+										</div>
+									</div>';
+					return '<div class="flex items-center gap-2">
+					' . $edit_button . ' ' . $delete_button . ' ' . $options_button . '</div';
+				})
+				->addColumn('image', function ($query) {
+					return "<img src='" . asset($query->image) . "' alt='" . $query->name . "' class='object-cover w-16'>";
+				})
+				->rawColumns(['image', 'action'])
+				->make(true);
+		}
 		return view('vendor.products');
 	}
 
--- resources/views/vendor/products.blade.php
+++ resources/views/vendor/products.blade.php
@@ -1,1 +1,122 @@
-<h1>Hi</h1>
+<x-vendor.layout.main page="Products">
+    <section class="flex flex-col flex-1 gap-6 px-4 py-4">
+        <div class="flex flex-col items-start justify-between w-full gap-2 sm:flex-row sm:items-center">
+            <h1 class="text-3xl text-gray-800 dark:text-gray-200">Products</h1>
+            <a href="{{ route('vendor.product.create') }}"
+                class="px-3 py-2 text-white bg-red-500 rounded shadow dark:bg-red-700 hover:bg-red-600">Add Product</a>
+        </div>
+        <div class="w-full" x-data="deleteItem">
+            <table class="w-full datatable">
+                <thead>
+                    <tr>
+                        <td> id </td>
+                        <td> image </td>
+                        <td> name </td>
+                        <td> price </td>
+                        <td> discount </td>
+                        <td> approved </td>
+                        <td> action </td>
+                    </tr>
+                </thead>
+                <tbody></tbody>
+            </table>
+
+        </div>
+    </section>
+
+    <div class="trash-table-modal" style="display: none">
+        <div class="trash-table-modal-content">
+            <i class="absolute w-5 h-5 bi bi-x top-2 right-2"></i>
+            <h2 class="text-xl text-gray-800 dark:text-gray-200">Are you sure you want to delete <span
+                    class="font-semibold capitalize delete-name"></span></h2>
+            <p class="text-sm text-gray-800 dark:text-gray-200">This will delete all variants and images
+                that have been added</p>
+            <div class="flex items-center self-end gap-2">
+                <button
+                    class="px-3 py-2 bg-gray-200 shadow-md delete-cancel-button dark:bg-gray-900 dark:text-gray-200">Cancel</button>
+                <form action="" method="POST" class="delete-form">
+                    @csrf
+                    @method('DELETE')
+                    <button class="px-3 py-2 text-white bg-red-500 rounded shadow-md dark:bg-red-700 delete-item"
+                        type="submit">Delete</button>
+                </form>
+            </div>
+        </div>
+    </div>
+
+
+    @section('script')
+        <script src="https://code.jquery.com/jquery-3.7.0.min.js"
+            integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
+        <script type="text/javascript">
+            let modal = $('.trash-table-modal')
+
+            modal.on('click', function(e) {
+                e.stopPropagation()
+                modal.hide()
+            })
+
+            $('.trash-table-modal-content').on('click', function(e) {
+                e.stopPropagation()
+            })
+
+            $('.delete-cancel-button').on('click', function(e) {
+                modal.hide()
+            })
+
+            $(function() {
+                let table = $('.datatable').DataTable({
+                    processing: true,
+                    serverside: true,
+                    ajax: "{{ route('vendor.product.index') }}",
+                    columns: [{
+                            data: 'id'
+                        },
+                        {
+                            data: 'image',
+                            orderable: false,
+                            searchable: false
+                        },
+                        {
+                            data: 'name'
+                        },
+                        {
+                            data: 'price'
+                        },
+                        {
+                            data: 'discount'
+                        },
+                        {
+                            data: 'approved'
+                        },
+                        {
+                            data: 'action',
+                            orderable: false,
+                            searchable: false,
+                        },
+                    ],
+                    drawCallback: function() {
+                        $('.deleteButton').on('click', function() {
+                            let {
+                                name,
+                                route
+                            } = this.dataset
+
+                            console.log(route)
+
+                            $('.delete-name').text(name)
+                            $('.delete-form').attr('action', route)
+                            modal.show()
+
+                            $('.delete-item').on('click', function() {
+                                this.submit()
+                            })
+                        })
+                    }
+
+                })
+            })
+        </script>
+    @endsection
+
+</x-vendor.layout.main>
