dd2767df9c59b4136270f68a85ffc0b0142c40d4
tonisco <tonyumeozulu@gmail.com>
2023-08-09 23:20:08 +0100

    image error fix

--- resources/views/components/general/input/image-input.blade.php
+++ resources/views/components/general/input/image-input.blade.php
@@ -4,7 +4,7 @@
             <div class="w-full h-full p-2">
                 <div
                     class="flex flex-col items-center justify-center w-full h-full gap-4 text-sm border-2 border-red-500 border-dashed rounded-lg cursor-pointer">
-                    <x-ri-image-2-line class="w-6 h-6 text-gray-500 sm:w-10 sm:h-10 dark:text-gray-200" />
+                    <x-ri-image-2-line class="w-6 h-6 text-gray-400 sm:w-8 sm:h-8 dark:text-gray-200" />
                 </div>
             </div>
         </template>
--- resources/views/vendor/Products/create.blade.php
+++ resources/views/vendor/Products/create.blade.php
@@ -59,13 +59,12 @@
 
                     <div
                         class="grid gap-4 grid-cols-[repeat(auto-fit,minmax(80px,1fr))] sm:grid-cols-[repeat(auto-fit,minmax(128px,1fr))] sm:auto-rows-[144px] auto-rows-[96px] grid-rows-[repeat(auto-fill,96px)] sm:grid-rows-[repeat(auto-fill,144px)]">
-
                         <div class="col-span-2 row-span-2">
                             <x-general.input.image-input name="image" id="image" />
+                            @error('image')
+                                <x-general.input.input-error :messages="$message" />
+                            @enderror
                         </div>
-                        @error('image')
-                            <x-general.input.input-error :messages="$message" />
-                        @enderror
 
                         @for ($i = 1; $i < 7; $i++)
                             <x-general.input.image-input name="product_image{{ $i }}"
@@ -125,9 +124,6 @@
                     </div>
                 </div>
 
-                <div class="flex flex-col gap-8 p-6 pb-8 bg-white rounded-lg shadow-md dark:bg-gray-800">
-                    <h2 class="text-lg font-medium text-gray-800 capitalize dark:text-gray-200">Variants</h2>
-                </div>
             </div>
 
 
